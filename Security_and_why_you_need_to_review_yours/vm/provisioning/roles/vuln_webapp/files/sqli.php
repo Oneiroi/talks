f62c66ec26bfc5c78aa0d07e90540931fb121db6
David Busby <oneiroi@fedoraproject.org>
2013-10-03 22:58:06 +0100

    sqli.php reworked, vuln_webapp role playbook wip mysql schema import

--- Security_and_why_you_need_to_review_yours/vm/provisioning/roles/vuln_webapp/files/sqli.php
+++ Security_and_why_you_need_to_review_yours/vm/provisioning/roles/vuln_webapp/files/sqli.php
@@ -1,8 +1,11 @@
+<pre>
 <?PHP
-
 $conn = mysql_connect(
-    '127.0.0.1',
+    'localhost',
     'vuln_webapp',
-    'pluk');
-
+    'pluk') or die('MySQL connection failed '.mysql_error());
+mysql_select_db('vuln_webapp');
+$res = mysql_query("SELECT * FROM users WHERE id = ".$_GET['q']) or die('Query failed '.mysql_error());
+print_r(mysql_fetch_assoc($res));
 ?>
+</pre>
