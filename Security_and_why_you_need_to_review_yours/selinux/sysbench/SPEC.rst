0c221cfd7f0a84aba2ba7397bfc3240495f29ee8
David Busby <oneiroi@fedoraproject.org>
2013-11-05 14:59:38 +0000

    Inconsistent results removing for the time being may revisit if time allows

--- Security_and_why_you_need_to_review_yours/selinux/sysbench/SPEC.rst
+++
@@ -1,4 +0,0 @@
-Vagrant VM, 
-backed onto SATA 1TB HDD 
-1GB Memory 
-8 Core AMD FX(tm)-8350 Eight-Core Processor
--- Security_and_why_you_need_to_review_yours/selinux/sysbench/bench.sh
+++
@@ -1,6 +0,0 @@
-sysbench --test=oltp --db-driver=mysql --mysql-db=test --mysql-user=root prepare
-for threads in 1 2 4 8 16 32;
-do
-sysbench --test=oltp --db-driver=mysql --mysql-db=test --mysql-user=root \
---num-threads=$threads run > threads-${threads}.out
-done
