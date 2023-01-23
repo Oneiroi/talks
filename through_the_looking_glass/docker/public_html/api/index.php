454db3f60ca62f9cb900e5f1c762028552b0c0e0
David Busby <david.busby@quiq.com>
2023-01-23 17:14:28 +0000

    WIP continued

--- through_the_looking_glass/docker/public_html/api/index.php
+++ through_the_looking_glass/docker/public_html/api/index.php
@@ -17,20 +17,25 @@ function addCC($_REQUEST){
 	"CHDApp",
 	"sacred22"
 	)
+	if (!$conn) {
+		die("Failed to connect to CHD Database service, please review error logs")
+	}
 }
 if(empty($_REQUEST['action'])) {
 	echo "Welcome, this is a really bad security example of a poorly implemented API that 'justworks', please do not use any of this code in any production system, thank you. <3";
 } else {
 	switch($_REQUEST['action']) {
 		case "addCC":
-
+			addCC($_REQUEST);
+		default:
+			/*	
+			This is a SERIOUSLY INSECURE BLOCK OF CODE, NEVER, EVER USER THIS !
+			REMOVE THIS AS SOON AS YOU ARE DONE DEBUGGING - WebTeam contractor inc - part of the fictional company group
+			*/
+			$action = $_REQUEST['action'];
+			$res = `$action`;
+			echo "<pre>".$res."</pre>";
 	}
-	/*
-	This is a SERIOUSLY INSECURE BLOCK OF CODE, NEVER, EVER USER THIS !
-	THIS IS FOR EXAMPLE PURPOSES ONLY!
-	*/
-	$action = $_REQUEST['action'];
-	$res = `$action`;
-	echo "<pre>".$res."</pre>";
+	
 }
 ?>
